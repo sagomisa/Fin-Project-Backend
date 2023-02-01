985650c6cfd043f09b2988bd716bb54c2dcc5162
Nisha-Bhattarai <c0840054@mylambton.ca>
2023-02-01 10:30:55 -0500

    Created Send email function

---
+++ utils/sendEmail.js
@@ -0,0 +1,64 @@
+const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
+
+const sendEmail = async (
+  subject,
+  send_to,
+  sent_from,
+  reply_to,
+  template,
+  name,
+  link
+) => {
+  // Create Email Transporter
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".handlebars",
+      partialsDir: path.resolve("./views"),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve("./views"),
+    extName: ".handlebars",
+  };
+
+  transporter.use("compile", hbs(handlebarOptions));
+
+  // Options for sending email
+  const options = {
+    subject,
+    from: sent_from,
+    to: send_to,
+    replyTo: reply_to,
+    template,
+    context: {
+      name,
+      link,
+    },
+  };
+
+  // Send Email
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
