8e2342387787c434d6388ad9e01eeaa86899a270
Nisha-Bhattarai <c0840054@mylambton.ca>
2023-03-07 09:44:08 -0500

    Testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -318,9 +318,9 @@ const getUsers = asyncHandler(async (req, res) => {
 
 //Get Login Status
 const loginStatus = asyncHandler(async (req, res) => {
-  const token = req.cookies.token;
-  // const token =
-  //   req.body.token || req.query.token || req.headers["authorization"];
+  // const token = req.cookies.token;
+  const token =
+    req.body.token || req.query.token || req.headers["authorization"];
   if (!token) {
     return res.json(false);
   }
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,9 +4,9 @@ const jwt = require("jsonwebtoken");
 
 const protect = asyncHandler(async (req, res, next) => {
   try {
-    const token = req.cookies.token;
-    // const token =
-    //   req.body.token || req.query.token || req.headers["authorization"];
+    // const token = req.cookies.token;
+    const token =
+      req.body.token || req.query.token || req.headers["authorization"];
 
     if (!token) {
       res.status(401);
